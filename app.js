b194f859a629e4a8d3d474d291c285860dfb1709
Muyiwa Akin-Ogundeji <oakinogundeji@gmail.com>
2017-04-21 09:32:44 +0100

    :package: implemented v1 of sematics3 API query

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const
   bParser = require('body-parser'),
   request = require('superagent'),
   encodeURL = require('urlencode'),
+  SEM3 = require('semantics3-node'),
   http = require('http'),
   app = express(),
   server = http.createServer(app);
@@ -29,7 +30,10 @@ const
   SECURITY_APPNAME = process.env.SECURITY_APPNAME,
   GLOBAL_ID = process.env.GLOBAL_ID,
   RESPONSE_DATA_FORMAT = process.env.RESPONSE_DATA_FORMAT,
-  PAGE_PAGINATION = process.env.PAGE_PAGINATION;
+  PAGE_PAGINATION = process.env.PAGE_PAGINATION,
+  SEM3_KEY = process.env.SEM3_KEY,
+  SEM3_SECRET = process.env.SEM3_SECRET,
+  SEM3_SERVICE = SEM3(SEM3_KEY, SEM3_SECRET);
 let searchURL = BASE_SEARCH_URL +'?OPERATION-NAME='+ OPERATION_NAME
   +'&SERVICE-VERSION='+ SERVICE_VERSION
   +'&SECURITY-APPNAME='+ SECURITY_APPNAME
@@ -68,6 +72,7 @@ app.use(bParser.urlencoded({extended: true}));
  */
 //=============================================================================
 app.get('/test', (req, res) => res.status(200).json('ok'));
+
 app.get('/searchebay', (req, res) => {
   const item = encodeURL(req.query.item);
   searchURL += '&keywords=' + item +'&paginationInput.entriesPerPage=' + PAGE_PAGINATION;
@@ -94,6 +99,34 @@ app.get('/searchebay', (req, res) => {
         return res.status(200).json(data.findItemsByKeywordsResponse[0].searchResult[0].item[0].primaryCategory[0].categoryName[0]);
       }
     });
+});
+
+app.get('/searchSEM3', (req, res) => {
+  const
+    endpoint = 'products',
+    method = 'GET',
+    queryObj = {search: ''},
+    item = req.query.item;
+  queryObj.search = item;
+  const queryObjStr = JSON.stringify(queryObj);
+  console.log('queryObjStr');
+  console.log(queryObjStr);
+  return SEM3_SERVICE.run_query(endpoint, queryObjStr, method, (err, products) => {
+    if(err) {
+      console.error(err);
+      return res.status(500).json({error: err});
+    } else {
+      console.log('products:');
+      console.log(products);
+      const
+        productsObj = JSON.parse(products),
+        sampleItem = productsObj.results[0],
+        brand = sampleItem.brand,
+        category = sampleItem.category;
+      return res.status(200).json({brand, category});
+    }
+  });
+
 });
 //=============================================================================
 /**
